1c6eec2f0840aeec9cb4a402709536f20ed101a4
Momin Haider <h@momin.dev>
2024-08-02 21:26:31 -0400

    Added hstack run testing

--- counterfactual_generation/hstack_run.sh
+++ counterfactual_generation/hstack_run.sh
@@ -2,15 +2,13 @@ nvidia-smi
 conda activate hf
 cd ~/SBoN
 
-accelerate launch \
---num_processes 1 \
---num_machines 1 \
---machine_rank 0 \
---mixed_precision no \
---dynamo_backend no \
-main.py \
---output_folder output_H100_BoN_AF_sft10k_ArmoRM-Llama3-8B-v0.1_20_8_seed_56 \
---llm_name sft10k \
---reward_model_name ArmoRM-Llama3-8B-v0.1 \
---max_tokens 2048 \
---max_gen_tokens 2048 --data_filename ./datasets/alpaca_farm_100.json --batch_size 20 --seed 56 --top_k 50 --top_p 1.0 --temperature 1.0
+python -m counterfactual_generation.generate \
+--data_filename ./datasets/alpaca_farm_100.json \
+--output_folder output_AF_gpt-xl__20_1000_seed_0 \
+--llm_name gpt-xl \
+--num_trajectories 1000 \
+--max_length 1024 \
+--batch_size 20 \
+--seed 0 \
+--top_k 50 \
+--top_p 1.0
