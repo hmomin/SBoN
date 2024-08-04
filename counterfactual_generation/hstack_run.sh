4b4fb36d5ead31543f940b16be4b9a483a640179
Momin Haider <h@momin.dev>
2024-08-04 16:50:57 -0400

    Modified hstack runner

--- counterfactual_generation/hstack_run.sh
+++ counterfactual_generation/hstack_run.sh
@@ -4,29 +4,29 @@ source /home/ubuntu/miniconda3/etc/profile.d/conda.sh
 conda activate hf
 cd /home/ubuntu/SBoN
 
-data_filename="./datasets/alpaca_farm_100.json"
-output_folder="output_AF_gpt2-xl__20_1000_seed_0"
-llm_name="gpt2-xl"
-num_trajectories=1000
-max_length=1024
-batch_size=20
-seed=0
-top_k=50
-top_p=1.0
+data_folder=(
+    "output_AF_gpt2-xl__20_1000_seed_0"
+)
+RM_names=(
+    "reward-model-deberta-v3-large-v2"
+    "RM-Mistral-7B"
+    "FsfairX-LLaMA3-RM-v0.1"
+    "ArmoRM-Llama3-8B-v0.1"
+    "reward-model-deberta-v3-large-v2"
+    "RM-Mistral-7B"
+    "FsfairX-LLaMA3-RM-v0.1"
+    "ArmoRM-Llama3-8B-v0.1"
+)
 device_ids=(0 1 2 3 4 5 6 7)
 
-for device_id in "${device_ids[@]}"
+for i in "${!device_ids[@]}"
 do
-    python -m counterfactual_generation.generate \
-    --data_filename $data_filename \
-    --output_folder $output_folder \
-    --llm_name $llm_name \
-    --num_trajectories $num_trajectories \
-    --max_length $max_length \
-    --batch_size $batch_size \
-    --seed $seed \
-    --top_k $top_k \
-    --top_p $top_p \
+    device_id=${device_ids[$i]}
+    RM_name=${RM_names[$i]}
+    
+    python -m counterfactual_generation.score \
+    --data_folder $data_folder \
+    --reward_model_name $RM_name \
     --device_id $device_id &
 done
 
