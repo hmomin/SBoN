b16c0a313a5b9cef1c7715e8e835af28a39ce332
Momin Haider <h@momin.dev>
2024-08-05 12:26:07 -0400

    Added remaining jobs for Llama-3-8B and rm-deberta

--- counterfactual_generation/hstack_run.sh
+++ counterfactual_generation/hstack_run.sh
@@ -5,25 +5,29 @@ conda activate hf
 cd /home/ubuntu/SBoN
 
 data_folders=(
+    # "output_AF_gpt2-xl__20_1000_seed_0"
     # "output_AF_gpt-j-6b__20_1000_seed_0"
-    # "output_AF_Meta-Llama-3-8B__20_1000_seed_0"
-    "output_AF_Mistral-7B-v0.3__20_1000_seed_0"
+    "output_AF_Meta-Llama-3-8B__20_1000_seed_0"
+    # "output_AF_Mistral-7B-v0.3__20_1000_seed_0"
 )
 RM_names=(
     "reward-model-deberta-v3-large-v2"
-    "RM-Mistral-7B"
-    "FsfairX-LLaMA3-RM-v0.1"
-    "ArmoRM-Llama3-8B-v0.1"
+    "reward-model-deberta-v3-large-v2"
+    "reward-model-deberta-v3-large-v2"
+    "reward-model-deberta-v3-large-v2"
+    # "RM-Mistral-7B"
+    # "FsfairX-LLaMA3-RM-v0.1"
+    # "ArmoRM-Llama3-8B-v0.1"
 )
 device_ids=(
-    0
-    1
-    2
-    3
-    # 4
-    # 5
-    # 6
-    # 7
+    # 0
+    # 1
+    # 2
+    # 3
+    4
+    5
+    6
+    7
 )
 
 index=0
