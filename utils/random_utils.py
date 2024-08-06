327aae093290f426dd1deca2da54c165b6e5c6d7
Momin Haider <h@momin.dev>
2024-08-05 21:16:45 -0400

    Autoformatting

--- utils/random_utils.py
+++ utils/random_utils.py
@@ -3,5 +3,5 @@ import time
 
 
 def reset_numpy_seed() -> None:
-    current_ms_time = int(time.time() * 1000) % (2**32)
+    current_ms_time = int(time.time() * 1000) % (2 ** 32)
     np.random.seed(current_ms_time)
