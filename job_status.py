ca7ea4cd81c438b6542cd7ea2373d90c688032eb
Momin Haider <h@momin.dev>
2024-08-06 12:24:41 -0400

    Adding job status for two job hoggers

--- job_status.py
+++ job_status.py
@@ -4,8 +4,12 @@ import subprocess
 import time
 from datetime import datetime
 from counterfactual_generation.generate_job_hogger import (
-    JOBS,
-    get_output_folder_from_tuple,
+    JOBS as generate_jobs,
+    get_output_folder_from_tuple as generate_get_output_folder_from_tuple,
+)
+from algorithm.spec_rej_job_hogger import (
+    JOBS as sr_jobs,
+    get_output_folder_from_tuple as sr_get_output_folder_from_tuple,
 )
 
 
@@ -44,9 +48,18 @@ def main() -> None:
         print(schedule_output.stdout[:-1])
         print(queue_output.stdout[:-1])
         print(datetime.now().strftime("%H:%M:%S"))
-        for key, job_list in JOBS.items():
+        for key, job_list in generate_jobs.items():
             for job_tuple in job_list:
-                output_folder = get_output_folder_from_tuple(job_tuple, key)
+                output_folder = generate_get_output_folder_from_tuple(job_tuple, key)
+                if not os.path.exists(output_folder):
+                    os.mkdir(output_folder)
+                num_files = len(os.listdir(output_folder))
+                if (no_empty and num_files == 0) or (no_full and num_files >= 100):
+                    continue
+                print(f"    {num_files} files in {output_folder}")
+        for key, job_list in sr_jobs.items():
+            for job_tuple in job_list:
+                output_folder = sr_get_output_folder_from_tuple(job_tuple, key)
                 if not os.path.exists(output_folder):
                     os.mkdir(output_folder)
                 num_files = len(os.listdir(output_folder))
